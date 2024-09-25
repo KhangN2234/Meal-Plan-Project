45bfe951c9f58f9abead6112f7661f1922c5d66c
Mark Nardone <tdy21@txstate.edu>
2024-09-25 18:33:21 -0500

    Basic UI interface created for account creation

--- meal_app/app.py
+++ meal_app/app.py
@@ -2,10 +2,13 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Simple route that shows a message
 @app.route('/')
 def home():
     return "Welcome to the Meal App!"
+
+@app.route('/signup', methods=['GET'])
+def signup():
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
