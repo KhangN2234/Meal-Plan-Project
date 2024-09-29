c96aee1400efe631038885dfb77a878513ad5dc1
Khang Ngo <vkw19@txstate.edu>
2024-09-28 21:03:38 -0500

    put all routes into routes.py

--- app.py
+++ app.py
@@ -1,42 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import sqlite3
 import bcrypt
+from meal_app import app
 
-app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return render_template('welcome.html')
-
-# Route to display the signup form and handle form submissions
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        
-        # Hash the password before storing it
-        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
-        
-        # It should store the username and password in the database but doesn't work
-        
-        conn = sqlite3.connect('users.db')
-        c = conn.cursor()
-        
-        try:
-            c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
-            conn.commit()
-            return redirect('/success')  # Redirect after successful signup
-        except sqlite3.IntegrityError:
-            return "Username already exists. Try another one."
-        finally:
-            conn.close()
-    
-    return render_template('signup.html')
-# Success page
-@app.route('/success')
-def success():
-    return "Account created successfully!"
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
