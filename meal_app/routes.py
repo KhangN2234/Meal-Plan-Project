53592824d5fcfee542e3504e75b236dcd9550050
Lucas Garcia <eqi1@txstate.edu>
2024-10-04 15:17:56 -0500

    CP-76 Added protein and ingredients data, various minor format changes

--- meal_app/routes.py
+++ meal_app/routes.py
@@ -1,5 +1,6 @@
 from meal_app import app
 from flask import Flask, render_template, request, redirect
+import json
 import sqlite3
 import bcrypt
 import requests
@@ -53,7 +54,7 @@ def search():
     else:
         searchbar = request.form['searchbar']
         mealtype = request.form['mealtype']
-        api_url = f"https://api.edamam.com/api/recipes/v2?type=any&q={searchbar}&app_id={recipe_search_app_id}&app_key={recipe_search_api_key}&mealType={mealtype}&random=false&field=uri&field=label&field=calories&field=yield"
+        api_url = f"https://api.edamam.com/api/recipes/v2?type=any&q={searchbar}&app_id={recipe_search_app_id}&app_key={recipe_search_api_key}&mealType={mealtype}&random=false&field=uri&field=label&field=calories&field=yield&field=ingredientLines&field=source&field=images&field=url&field=totalNutrients"
 
         response = requests.get(api_url)
 
@@ -61,7 +62,7 @@ def search():
 
         list_of_recipes = data['hits']
 
-        display_data = [{'label': recipe['recipe']['label'], 'calories': round(recipe['recipe']['calories']), 'servings': round(recipe['recipe']['yield']), 'cal_per_serv': round(recipe['recipe']['calories']/recipe['recipe']['yield'])} for recipe in list_of_recipes]
+        display_data = [{'label': recipe['recipe']['label'], 'calories': round(recipe['recipe']['calories']), 'servings': round(recipe['recipe']['yield']), 'cal_per_serv': round(recipe['recipe']['calories']/recipe['recipe']['yield']), 'ingredients': recipe['recipe']['ingredientLines'], 'url': recipe['recipe']['url'], 'source': recipe['recipe']['source'], 'protein': round(recipe['recipe']['totalNutrients']['PROCNT']['quantity']), 'proteinunit': recipe['recipe']['totalNutrients']['PROCNT']['unit'], 'protein_per_serv': round(recipe['recipe']['totalNutrients']['PROCNT']['quantity']/recipe['recipe']['yield'])} for recipe in list_of_recipes]
         
 
         return render_template('search.html', recipes=display_data, mealtype=mealtype, searchbar=searchbar, success=True)
