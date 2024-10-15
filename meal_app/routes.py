d75dfeb9874f29261c6afeeb907cad40a9b79954
Khang Ngo <vkw19@txstate.edu>
2024-10-15 14:30:02 -0500

    SCRUM-82: Added diet as parameter

--- meal_app/routes.py
+++ meal_app/routes.py
@@ -62,7 +62,8 @@ def search():
         dishtype = request.form['dishtype']
         maxIngredients = request.form['maxIngredients']
         cuisineType = request.form['cuisinetype']
-        api_url = f"https://api.edamam.com/api/recipes/v2?type=any&q={searchbar}&app_id={recipe_search_app_id}&app_key={recipe_search_api_key}&mealType={mealtype}&dishType={dishtype}&ingr={maxIngredients}&cuisineType={cuisineType}&random=false&field=uri&field=label&field=calories&field=yield&field=ingredientLines&field=source&field=images&field=url&field=totalNutrients"
+        dietType = request.form['diet']
+        api_url = f"https://api.edamam.com/api/recipes/v2?type=any&q={searchbar}&app_id={recipe_search_app_id}&app_key={recipe_search_api_key}&mealType={mealtype}&dishType={dishtype}&ingr={maxIngredients}&cuisineType={cuisineType}&diet={dietType}&random=false&field=uri&field=label&field=calories&field=yield&field=ingredientLines&field=source&field=images&field=url&field=totalNutrients"
 
         response = requests.get(api_url)
 
