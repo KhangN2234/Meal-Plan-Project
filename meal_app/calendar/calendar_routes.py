a88b5fd9bc9ca2622b76b59504bd9483ff340743
Khang Ngo <vkw19@txstate.edu>
2024-10-21 01:52:42 -0500

    Basic CSS and HTML
    Only Monday and Tuesday for now

---
+++ meal_app/calendar/calendar_routes.py
@@ -0,0 +1,17 @@
+from meal_app import app
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+import re
+
+calendar_templates = Blueprint('calendar',__name__)
+
+@app.route('/calendar', methods=['GET', 'POST'])
+def calendar():
+    if request.method == 'GET': 
+        return render_template('calendar.html')
+    else:
+        recipe_label = request.form.get('recipe_label')
+        recipe_url = request.form.get('url')
+
+    # You can now display the recipe name and URL on the calendar page
+    return render_template('calendar.html', recipe_label=recipe_label, recipe_url=recipe_url)
