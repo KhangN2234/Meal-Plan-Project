c5622a98a0902e7525f7517e574b4f046bd1cb3e
Khang Ngo <vkw19@txstate.edu>
2024-10-21 02:12:12 -0500

    Links now takes user to recipe

--- meal_app/calendar/calendar_routes.py
+++ meal_app/calendar/calendar_routes.py
@@ -11,7 +11,7 @@ def calendar():
         return render_template('calendar.html')
     else:
         recipe_label = request.form.get('recipe_label')
-        recipe_url = request.form.get('url')
+        recipe_url = request.form.get('recipe_url')
 
     # You can now display the recipe name and URL on the calendar page
     return render_template('calendar.html', recipe_label=recipe_label, recipe_url=recipe_url)
