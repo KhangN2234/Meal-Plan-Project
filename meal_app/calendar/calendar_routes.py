f27c52f427f83ffdb8a094632fd67190aae0450b
Khang Ngo <vkw19@txstate.edu>
2024-10-21 02:31:02 -0500

    Turn to checkbox
    Links can now appear in Monday and Tuesday

--- meal_app/calendar/calendar_routes.py
+++ meal_app/calendar/calendar_routes.py
@@ -7,11 +7,10 @@ calendar_templates = Blueprint('calendar',__name__)
 
 @app.route('/calendar', methods=['GET', 'POST'])
 def calendar():
-    if request.method == 'GET': 
-        return render_template('calendar.html')
-    else:
-        recipe_label = request.form.get('recipe_label')
-        recipe_url = request.form.get('recipe_url')
+    # Access the form data
+    recipe_label = request.form.get('recipe_label')
+    recipe_url = request.form.get('recipe_url')
+    selected_days = request.form.getlist('selected_days')  # Get a list of selected days
 
-    # You can now display the recipe name and URL on the calendar page
-    return render_template('calendar.html', recipe_label=recipe_label, recipe_url=recipe_url)
+    # Pass the selected days and recipe details to the template
+    return render_template('calendar.html', recipe_label=recipe_label, recipe_url=recipe_url, selected_days=selected_days)
