6808478105599d7968389f481b7d9c0bbfdcea64
Khang Ngo <vkw19@txstate.edu>
2024-10-29 23:19:06 -0500

    Input recipe to user when logged in

--- meal_app/calendar/calendar_routes.py
+++ meal_app/calendar/calendar_routes.py
@@ -1,6 +1,7 @@
-from meal_app import app
-from flask import Flask, render_template, request, redirect
+from meal_app import app, db
+from flask import Flask, render_template, request, redirect, session
 from flask import Blueprint
+from firebase_admin import firestore
 import re
 
 calendar_templates = Blueprint('calendar',__name__)
@@ -8,11 +9,22 @@ calendar_templates = Blueprint('calendar',__name__)
 @app.route('/calendar', methods=['GET', 'POST'])
 def calendar():
     if request.method == 'GET': 
+        if 'user' in session:
+            # Structure to save in Firebase
+            print("success")
         return render_template('calendar.html')
     else:
-        recipe_label = request.form.get('recipe_label')
-        recipe_url = request.form.get('recipe_url')
-        selected_days = request.form.getlist('selected_days')  # Get a list of selected days
-
-        # Pass the selected days and recipe details to the template
-        return render_template('calendar.html', recipe_label=recipe_label, recipe_url=recipe_url, selected_days=selected_days)
+        if 'user' in session:
+            recipe_label = request.form.get('recipe_label')
+            recipe_url = request.form.get('recipe_url')
+            selected_days = request.form.getlist('selected_days')  # Get a list of selected days
+            email = session['user']
+            recipe_data = {
+                'recipe_label': recipe_label,
+                'recipe_url': recipe_url,
+                'days': selected_days
+            }
+            db.collection('users').document(email).collection('recipes').add(recipe_data)
+            
+            # Pass the selected days and recipe details to the template
+            return render_template('calendar.html', recipe_label=recipe_label, recipe_url=recipe_url, selected_days=selected_days)
