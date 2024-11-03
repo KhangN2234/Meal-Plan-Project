b11213d55298a6eb6f33831fdac3d77b579aa3e4
Khang Ngo <vkw19@txstate.edu>
2024-11-02 19:18:46 -0500

    SCRUM-95: bug fix
    add to calendar button was broken due to how multiple recipe was implemented

--- meal_app/calendar/calendar_routes.py
+++ meal_app/calendar/calendar_routes.py
@@ -32,7 +32,6 @@ def calendar():
                      'days' : recipes_collection_ref.document(recipe).get().to_dict().get('days'),
                      'url' : recipes_collection_ref.document(recipe).get().to_dict().get('recipe_url')} for recipe in recipes_name]
             
-            #recipes_list = [{'label': db.collection('users').document(email).collection('recipes').document(recipe).get().to_dict().get('recipe_label')} for recipe in recipes_name]
             return render_template('calendar.html', 
                                    recipes_list = data
                                    )
@@ -41,19 +40,51 @@ def calendar():
     
     else:
         if 'user' in session:
+            email = session['user']
+
+            # Reference to the user's recipes collection
+            recipes_collection_ref = db.collection('users').document(email).collection('recipes')
+            recipes_docs = recipes_collection_ref.stream()
+            doc = db.collection('users').document(email).collection('recipes').get()
+            
+            # Check if the 'recipes' collection is empty
+            if not any(True for _ in recipes_docs):
+                return redirect('search')
+            
+    # This portion add the new recipe to the firebase 
             recipe_label = request.form.get('recipe_label')
             recipe_url = request.form.get('recipe_url')
-            selected_days = request.form.getlist('selected_days')  # Get a list of selected days
-            email = session['user']
-            if 'recipes' != recipe_label:
-                recipe_data = {
-                    'recipe_label': recipe_label,
-                    'recipe_url': recipe_url,
-                    'days': selected_days
-                }
-                db.collection('users').document(email).collection('recipes').document(recipe_label).set(recipe_data)
-            else:
-                db.collection('users').document(email).collection('recipes').document(recipe_label).array('days').set(selected_days)
-                
-            # Pass the selected days and recipe details to the template
-            return render_template('calendar.html', recipe_label=recipe_label, recipe_url=recipe_url, selected_days=selected_days)
+            selected_days = request.form.getlist('selected_days')
+            
+            recipe_data = {
+                'recipe_label': recipe_label,
+                'recipe_url': recipe_url,
+                'days': selected_days
+            }
+            
+            # Save to firebase
+            recipes_collection_ref.document(recipe_label).set(recipe_data)
+            
+    # This portion will display the data in the firebase
+            # Put name of recipes in an array list
+            recipes_name = []
+            #.to_dict() convert the variable to an array then .get() get the value (ie recipe_url) from that map created from the document
+            for recipe in doc:
+                recipes_name.append(recipe.to_dict().get('recipe_label'))
+
+
+            data = [{'label': recipes_collection_ref.document(recipe).get().to_dict().get('recipe_label'), 
+                     'days' : recipes_collection_ref.document(recipe).get().to_dict().get('days'),
+                     'url' : recipes_collection_ref.document(recipe).get().to_dict().get('recipe_url')} for recipe in recipes_name]
+
+
+            return render_template('calendar.html', 
+                                   recipes_list = data,
+                                   recipe_label = recipe_label,
+                                   recipe_url = recipe_url,
+                                   selected_days = selected_days
+                                   )
+        else:
+            return redirect('/login')
+            
+            
