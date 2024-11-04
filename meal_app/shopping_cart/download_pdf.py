72329a2eded3e34257f3896aad96ddbab6115eaa
Lucas Garcia <eqi1@txstate.edu>
2024-11-03 20:55:41 -0600

    SCRUM-89 Added algorithm to process api data

--- meal_app/shopping_cart/download_pdf.py
+++ meal_app/shopping_cart/download_pdf.py
@@ -17,3 +17,26 @@ def download_pdf():
         user_data = doc_ref.get().to_dict()
         saved_recipes = user_data.get('cart', [])
         
+        all_ingredients = []
+
+        for recipe_uri in saved_recipes:
+            # Call the API for each URI to get recipe details (if needed)
+            encoded_string = quote(recipe_uri, safe='')
+            api_url = f"https://api.edamam.com/api/recipes/v2/by-uri?type=public&app_id={recipe_search_app_id}&app_key={recipe_search_api_key}&uri={encoded_string}"
+            response = requests.get(api_url)
+            data = response.json()
+            list_of_recipes = data.get('hits', [])
+
+            for recipe in list_of_recipes:
+                recipe_label = recipe['recipe']['label']
+                for item in recipe['recipe']['ingredients']:
+                    measure = item.get('measure', "")
+                    if measure == "<unit>":
+                        measure = "​"
+                    processed_item = {
+                        'quantity': round(item.get('quantity', 0), 2),
+                        'measure': measure,
+                        'food': item.get('food', "Unknown"),
+                        'recipe': recipe_label
+                    }
+                    all_ingredients.append(processed_item)
