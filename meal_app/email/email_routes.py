df1414d19d9f207f7a8c735cecbde808d05265b1
Joseph Bond <jmb511@txstate.edu>
2024-11-26 21:22:25 -0600

    SCRUM-125: began building email sending route
    route is not complete yet, email does not send correctly

--- meal_app/email/email_routes.py
+++ meal_app/email/email_routes.py
@@ -14,4 +14,38 @@ def send_email():
     if 'user' not in session:
         flash("You need to log in to send an email.")
         return redirect ('/login')
+
+    #Get the logged-in user's email
+    email = session['user']
+    doc_ref = db.collection('users').document(email)
+    doc = doc_ref.get()
+
+    if not doc.exists:
+        flash('user information not found')
+        return redirect('/profile')
     
+    user_data = doc.to_dict()
+    recipient_email = user_data.get('email')
+
+    if not recipient_email:
+        flash('No email address found for teh user.')
+        return redirect("/profile")
+    
+    try:
+        #Email content
+        sender_email = "MealApp0@gmail.com"
+        app_password = "cqal ozgh cofo tjvt"
+        subject = "Daily Prep Reminder!"
+        body = """Hello1,
+
+        Here are the meals you have scheduled today!
+
+        Regards,
+        Meal App Team"""
+
+        msg = MIMEMultipart()
+        msg['From'] = sender_email
+        msg['To'] = recipient_email
+    except Exception as e:
+        flash(f"Failed to send email: {str(e)}")
+    return redirect('/profile')
